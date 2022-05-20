eed99ffb95d69d92565c79992777252c3708b132
Staff SIESTA <staff@siesta.id>
2022-05-20 15:42:04 +0700

    first

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { useEffect } from 'react'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap.bundle.min.js')
+  }, [])
+  return (
+    <>
+      <Head>
+        <title>{Component.title}</title>
+      </Head>
+        <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
